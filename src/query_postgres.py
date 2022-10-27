96edb2639a5055bf69ff9ea231e8626cb033bbc3
edededd <luis.chahua@utec.edu.pe>
2022-10-27 13:43:23 -0400

    postgres done

---
+++ src/query_postgres.py
@@ -0,0 +1,85 @@
+import sqlalchemy as sa
+from sqlalchemy_utils import database_exists, create_database
+import time
+import json, os
+import pandas as pd
+import csv
+# Reading credentials
+
+db_user = "postgres" ## poner el user del docker
+db_pass = "qwertyu"  ## poner el password del docker
+dirPath = "/home/luischahua/data/arxiv"  ## poner el directorio donde estan los abstracts
+
+## colocar (id,abstract) en un csv
+dirCSV ='/home/luischahua/papers_arxiv.csv'
+
+
+
+uri = f'postgresql://{db_user}:{db_pass}@localhost:5432/noticias'
+
+engine = sa.create_engine(uri)
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+with engine.connect().execution_options(autocommit=True) as connection:
+    data = connection.execute(sa.sql.text("""
+            CREATE TABLE IF NOT EXISTS papers (
+                id text PRIMARY KEY,
+                abstract text
+            );
+            alter table papers add column if not exists content_ts tsvector;
+            create index if not exists idx_content_ts on papers using gin (content_ts);
+    """))
+
+    data = connection.execute(sa.sql.text("""
+            TRUNCATE TABLE papers
+    """))
+
+    data = connection.execute(sa.sql.text(f"""
+            COPY papers(id,abstract) 
+            FROM '{dirCSV}'
+            DELIMITER ','
+            CSV HEADER;
+    """))
+    print(data)
+
+    data = connection.execute(sa.sql.text("""
+            update papers
+            set content_ts = x.content_ts
+            from (
+            select Id, 
+                    setweight(to_tsvector('english', abstract), 'A')
+                    as content_ts
+            from papers
+            ) as x
+            where x.Id = papers.Id;
+    """))
+
+
+    print(data)
+
+def TopK_answer(text, k):
+    with engine.connect().execution_options(autocommit=True) as connection:
+        ans_all = []
+        start = time.time()
+        data = connection.execute(sa.sql.text(f""" 
+            select abstract,ts_rank_cd(content_ts, query_ts) as score
+            from papers, to_tsquery('english', '{text}') query_ts
+            where query_ts @@ content_ts
+            order by score desc 
+            limit {k};
+        """))
+        end = time.time()
+        total_time = round(end - start, 8)       
+        for row in data:
+            dict1 = {}
+            dict1['abstract'] = row['abstract']
+            dict1['score'] = row['score']
+            ans_all.append(dict1)
+
+        response = { "ans_all": ans_all, "total_time": total_time } 
+
+    return response
+
+print(TopK_answer("paper",6))
--- src/splitArxiv.py
+++ src/splitArxiv.py
@@ -1,9 +1,10 @@
-import ijson
-import os
+import json, os
+import pandas as pd
+import csv
 
 
-jsonPath = "/data/arxiv-metadata-oai-snapshot.json"
-dirPath = "/data/arxiv"
+jsonPath = "/home/luischahua/data/arxiv-metadata-oai-snapshot.json"
+dirPath = "/home/luischahua/data/arxiv"
 
 if not os.path.exists(dirPath):
     os.makedirs(dirPath)
@@ -26,3 +27,24 @@ for prefix, event, value in parser:
 		with open(path,"w") as f:
 			f.write(abstract)
 		id,abstract = "",""
+
+dirPath = "/home/luischahua/data/arxiv"
+dirCSV ='/home/luischahua/papers_arxiv.csv'
+field_names = ['id','abstract']
+dataframe = pd.DataFrame(list())
+dataframe.to_csv(dirCSV)
+
+j = open(dirCSV,'w')
+writer = csv.writer(j)
+writer.writerow(field_names)
+j.close()
+for filename in os.listdir(dirPath):
+    f = os.path.join(dirPath, filename)
+    f1 = str(f)
+    id1 = f1[-9:]
+
+    f2 = open(f,'r')
+    abstract1 = f2.read()
+    with open(dirCSV, 'a') as csv_file:
+        dict_object = csv.DictWriter(csv_file, fieldnames=field_names) 
+        dict_object.writerow({'id':id1,'abstract':abstract1})
