2e045c5aaafa8162461ceeff0e6c5463e27f343f
warleon <51892390+warleon@users.noreply.github.com>
2022-10-30 16:49:55 -0500

    finished indexing script

--- src/invertedIndex.py
+++ src/invertedIndex.py
@@ -1,6 +1,6 @@
 import json 
 import os
-
+import re
 import nltk
 import numpy as np
 from nltk.stem.snowball import SnowballStemmer
@@ -10,21 +10,23 @@ class InvertedIndex:
 	def __init__(self,indexDir):
 		nltk.download('punkt')
 		self.indexDir = indexDir
-		self.stoplist = [',', '!', '.', '?', '-', ';','"','¿',')','(','[',']',' ']
+		self.stoplist = r"[\W+\d+_]"
 		self.stopWords = set(["the","of"])
 		self.stemmer = SnowballStemmer('english')
-		self.indexPath = os.path.join(indexDir,"index")
+		self.indexPath = os.path.join(indexDir,"index.meta")
 		self.N = 0
+		if not os.path.exists(self.indexDir):
+			os.makedirs(self.indexDir)
 		if os.path.exists(self.indexPath):
 			ifile = open(self.indexPath,"r")
 			ijson = json.load(ifile)
 			self.N = ijson["N"]
 			ifile.close()
+		
 	
 	def __del__(self):
 		ifile = open(self.indexPath,"w")
-		ijson = json({"N":self.N})
-		json.dump(ijson,ifile)
+		json.dump({"N":self.N},ifile)
 		ifile.close()
 
 
@@ -33,7 +35,7 @@ class InvertedIndex:
 	
 	def processText(self,text):
 		res = []
-		for word in text.split(self.stoplist):
+		for word in re.split(self.stoplist,text):
 			if word in self.stopWords:
 				continue
 			res.append(self.processWord(word))
@@ -47,13 +49,15 @@ class InvertedIndex:
 		with open(docPath, "r") as doc:
 			for line in doc:
 				for pword in self.processText(line):
+					if not len(pword):
+						continue
 					uniqWords.add(pword)
 					wpath=os.path.join(self.indexDir,pword)
 					winfo = None
 					data = {}
 					#check if the word was already indxed
 					if os.path.exists(wpath):
-						winfo = open(wpath, "rw") 
+						winfo = open(wpath, "r") 
 						data = json.load(winfo)
 					else:
 						winfo=open(wpath, "w")
@@ -64,22 +68,27 @@ class InvertedIndex:
 					else:
 						data["termfreq"][docPath] = 1
 
+					winfo.close()
+					winfo = open(wpath, "w") 
 					json.dump(data,winfo)
 					winfo.close()
 			for uw in uniqWords:
 				uwpath=os.path.join(self.indexDir,uw)
-				uwfile = open(uwpath, "rw") 
+				uwfile = open(uwpath, "r") 
 				uwjson = json.load(uwfile)
 				if "docfreq" in uwjson:
 					uwjson["docfreq"] += 1
 				else:
 					uwjson["docfreq"] = 1
+				uwfile.close()
+				uwfile = open(uwpath, "w") 
 				json.dump(uwjson,uwfile)
 				uwfile.close()
 
 	def index(self,dirpath):
-		for filename in os.listdir(dirpath):
-			self.addDocument(filename)
+		for root, dirs, files in os.walk(dirpath):
+			for filename in files:
+					self.addDocument(os.path.join(root, filename))
 	
 	#word being a dictionary with the tf and df and doc being a document path/filename
 	def tf_idf(self, word,doc):
--- src/splitArxiv.py
+++ src/splitArxiv.py
@@ -1,10 +1,8 @@
 import json, os
-import pandas as pd
-import csv
 import ijson
 
-jsonPath = "/home/luischahua/data/arxiv-metadata-oai-snapshot.json"
-dirPath = "/home/luischahua/data/arxiv"
+jsonPath = "/data/arxiv-metadata-oai-snapshot.json"
+dirPath = "/data/arxiv"
 
 if not os.path.exists(dirPath):
     os.makedirs(dirPath)
@@ -14,6 +12,7 @@ parser = ijson.parse(file,multiple_values=True)
 
 inAbstract,inId = False,False
 id,abstract = "",""
+count=0
 for prefix, event, value in parser:
 	if event=="map_key" and value == "id":
 		p,e,v = next(parser)
@@ -27,6 +26,16 @@ for prefix, event, value in parser:
 		with open(path,"w") as f:
 			f.write(abstract)
 		id,abstract = "",""
+		count+=1
+	if count >=100:
+		break
+#chahua indexing :v
+"""
+import pandas as pd
+import csv
+jsonPath = "/home/luischahua/data/arxiv-metadata-oai-snapshot.json"
+dirPath = "/home/luischahua/data/arxiv"
+
 
 dirPath = "/home/luischahua/data/arxiv"
 dirCSV ='/home/luischahua/papers_arxiv.csv'
@@ -48,3 +57,4 @@ for filename in os.listdir(dirPath):
     with open(dirCSV, 'a') as csv_file:
         dict_object = csv.DictWriter(csv_file, fieldnames=field_names) 
         dict_object.writerow({'id':id1,'abstract':abstract1})
+"""
